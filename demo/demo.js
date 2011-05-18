f2d56ef374dc86c4b4e8103922bec1f2af0f301c
Mark Malek <markmmalek@me.com>
2011-05-18 01:07:14 -0400

    Update demo:
    Updated demo file with new examples.

--- demo/demo.js
+++ demo/demo.js
@@ -1,20 +1,11 @@
 $(document).ready(function() {
-    $('.myTable01').fixedHeaderTable({ width: '600', height: '250', footer: true, cloneHeadToFoot: true, altClass: 'odd', themeClass: 'fancyTable', autoShow: false });
+    $('.myTable01').fixedHeaderTable({ height: '250', footer: true, cloneHeadToFoot: true, altClass: 'odd', themeClass: 'fancyTable', autoShow: false });
     
     $('.myTable01').fixedHeaderTable('show', 1000);
     
-    $('.myTable02').fixedHeaderTable({ width: '600', height: '250', footer: true, altClass: 'odd', themeClass: 'fancyDarkTable' });
+    $('.myTable02').fixedHeaderTable({ height: '250', footer: true, altClass: 'odd', themeClass: 'fancyTable' });
     
-    $('.myTable03').fixedHeaderTable({ width: '400', height: '400', altClass: 'odd', footer: true, fixedColumn: true, themeClass: 'fancyTable' });
+    $('.myTable03').fixedHeaderTable({ height: '400', altClass: 'odd', footer: true, fixedColumn: true, themeClass: 'fancyTable' });
     
-	$('a.makeTable').bind('click', function() {
-		
-
-		$('.myTable01').fixedHeaderTable('destroy');
-		
-		$('.myTable01 th, .myTable01 td')
-			.css('border', $('#border').val() + 'px solid ' + $('#color').val());
-			
-		$('.myTable01').fixedHeaderTable({ width: $('#width').val(), height: $('#height').val(), footer: true, themeClass: 'fancyTable' });
-	});
+    $('.myTable04').fixedHeaderTable({ height: '400', altClass: 'odd', footer: true, cloneHeadToFoot: true, fixedColumn: true, themeClass: 'fancyTable' });
 });

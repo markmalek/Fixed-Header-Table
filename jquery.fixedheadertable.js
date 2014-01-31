e7aa5be530f145bdadf5bb1cf91cff5e8342cc82
hiddes <dmishin.cae@gmail.com>
2014-01-31 14:51:53 +0700

    FIXED ISSUE #69 Scrolling over fixed column header distorts the table

--- jquery.fixedheadertable.js
+++ jquery.fixedheadertable.js
@@ -463,7 +463,7 @@
 
         // bind mousewheel events
         var maxTop = $fixedColumn.find('.fht-tbody .fht-table').height() - $fixedColumn.find('.fht-tbody').height();
-        $fixedColumn.find('.fht-table').bind('mousewheel', function(event, delta, deltaX, deltaY) {
+        $fixedColumn.find('.fht-tbody .fht-table').bind('mousewheel', function(event, delta, deltaX, deltaY) {
           if (deltaY == 0) {
             return;
           }
